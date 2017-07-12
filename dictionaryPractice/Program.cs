7e3f9c7cc5d3f50df2fd6e558bd78f74e31004db
Jay <isaacsdaddy42293@gmail.com>
2017-07-12 14:18:07 -0400

    dictionarypractice

--- dictionaryPractice/Program.cs
+++ dictionaryPractice/Program.cs
@@ -9,7 +9,39 @@ namespace dictionaryPractice
     class Program
     {
         static void Main(string[] args)
-        { 
+        {
+            //Dictionary<string, string> snackMachine = new Dictionary<string, string>()
+            //{
+            //    {"A1", "Twix" },
+            //    {"A2", "Twizzlers" },
+            //    {"A3", " Reeses" },
+            //    {"A4", "Sour Patch Kids" }
+            //};
+            //foreach(KeyValuePair<string, string> item in snackMachine)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //Dictionary<int, string> drinkMachine = new Dictionary<int, string>();
+
+            ////using add method to add drinks
+
+            //drinkMachine.Add(10, "Ginger Ale");
+            //drinkMachine.Add(11, "Disani");
+            //drinkMachine.Add(12, "Orange Juice");
+            //drinkMachine.Add(13, "Cherry Coke");
+            //drinkMachine.Add(14, "Pepsi");
+            ////Counting number of entrys in the dictionary
+            //// Console.WriteLine(drinkMachine.Count);
+
+            //foreach  (KeyValuePair<int,string> drink in drinkMachine)
+            //{
+            //    Console.WriteLine(drink.Key);
+            //    Console.WriteLine(drink.Value);
+            //}
+
+
+
         }
     }
 }
